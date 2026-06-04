bc576ae1cb6de51856b41757c76a672600a2fb37
jearson-roa <jearson.roa@gmail.com>
2026-06-04 18:56:38 -0400

    Primera parte del ejercicio 1

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,1 +1,51 @@
-print("Este es mi primer desarrollo utilizando git 01")
+
+lista_productos = []
+
+print("***** Sistema Fruna *****");
+print("** Vers - 0.0.1 **");
+
+pedido = input("Desea agregar productos al listado?: ").upper()
+
+if pedido == "SI":
+        productos = input("Nombre del producto: ")
+        precios = int(input("Precio del producto: "))
+
+        nuevo_prod = {
+                "Producto": productos,
+                "Precio": precios
+        }
+
+        lista_productos.append(nuevo_prod)
+        print("n\Producto añadido correctamente")
+
+        confirmacion = input("Desea agregar otro producto?: ").upper()
+
+
+        while confirmacion == "APARTE":
+            productos = input("Nombre del producto: ")
+            precios = int(input("Precio del producto: "))
+
+            nuevo_prod = {
+                  "Producto": productos,
+                  "Precio": precios
+            }
+
+            lista_productos.append(nuevo_prod)
+            print("n\Producto añadido correctamente")
+
+
+        if confirmacion == "AHINOMAS":
+                print("Ha dejado de agregar productos.")
+                break
+
+print("*** Lista de productos ***")
+for i in lista_productos:
+    print("Producto:", i["Producto"]) 
+    print("Precio:", i["Precio"])
+    print("---")
+
+    
+
+        
+
+
