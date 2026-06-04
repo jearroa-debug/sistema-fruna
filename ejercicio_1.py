e68c357823b38ab27c51a178fa93e4bd5a99e963
jearson-roa <jearson.roa@gmail.com>
2026-06-04 19:15:03 -0400

    archivo actualizado

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -4,9 +4,12 @@ lista_productos = []
 print("***** Sistema Fruna *****");
 print("** Vers - 0.0.1 **");
 
-pedido = input("Desea agregar productos al listado?: ").upper()
+pedido = input("\nDesea crear lista de productos?: ").upper()
 
 if pedido == "SI":
+        print("\n*** Iniciando lista de productos ***")
+        print("------------------------------------------")
+
         productos = input("Nombre del producto: ")
         precios = int(input("Precio del producto: "))
 
@@ -16,30 +19,35 @@ if pedido == "SI":
         }
 
         lista_productos.append(nuevo_prod)
-        print("n\Producto añadido correctamente")
+        print("------------------------------------------")
+        print("\nProducto añadido correctamente")
+        
+        while True:
+            confirmacion = input("\nDesea agregar otro producto?: ").upper()
 
-        confirmacion = input("Desea agregar otro producto?: ").upper()
+            if confirmacion == "APARTE":
+
+                productos = input("Nombre del producto: ")
+                precios = int(input("Precio del producto: "))
+
+                nuevo_prod = {
+                    "Producto": productos,
+                    "Precio": precios
+                }
+
+                lista_productos.append(nuevo_prod)
+                print("\nProducto añadido correctamente")
 
 
-        while confirmacion == "APARTE":
-            productos = input("Nombre del producto: ")
-            precios = int(input("Precio del producto: "))
-
-            nuevo_prod = {
-                  "Producto": productos,
-                  "Precio": precios
-            }
-
-            lista_productos.append(nuevo_prod)
-            print("n\Producto añadido correctamente")
-
-
-        if confirmacion == "AHINOMAS":
+            elif confirmacion == "AHINOMAS":
                 print("Ha dejado de agregar productos.")
                 break
+            else:
+                print("Opciones no validas")
 
-print("*** Lista de productos ***")
+print("\n*** Lista de productos ***")
 for i in lista_productos:
+    print("---")
     print("Producto:", i["Producto"]) 
     print("Precio:", i["Precio"])
     print("---")
